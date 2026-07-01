12b71d66827b70f24f61f652add85067ae522bc3
ernanifs <66725719+ernanifs@users.noreply.github.com>
2026-07-01 15:52:09 -0300

    Delete NivelColaborador.kt

--- NivelColaborador.kt
+++
@@ -1,8 +0,0 @@
-package br.com.treinamento.modulojetpackcompose.projetoCrudColaboradoes.modelos
-
-enum class NivelColaborador(val descricao: String) {
-    ADMINISTRATIVO("Administrativo"),
-    FINANCEIRO("Financeiro"),
-    GERENCIA("Gerencia"),
-    SUPORTE("Suporte")
-}
